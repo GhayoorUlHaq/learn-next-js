a10fd52c83e27c0c60200f3abd0576fe4ccc95f1
GhayoorUlHaq <ghayoor10th@gmail.com>
2021-10-03 16:21:17 +0500

    Added api configuration and server settings

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout from "../components/Layout";
 import Articles from "../components/Articles";
+import { server } from '../config'
 
 export default function Home({articles}) {
     console.log(articles)
@@ -21,7 +22,7 @@ export default function Home({articles}) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
   return {
     props: {
@@ -29,3 +30,13 @@ export const getStaticProps = async () => {
     },
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
